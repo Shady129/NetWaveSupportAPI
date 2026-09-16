73f5638a36703c231692837a4d3ac157f7abc4a2
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-09-16 13:38:38 +0300

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace NetWaveSupportAPI.Models
+{
+   
+        public class Customer
+        {
+            public int Id { get; set; }
+
+            public string FullName { get; set; }
+
+            public string Email { get; set; }
+
+            public string PasswordHash { get; set; }
+
+            public string Role { get; set; }
+
+            public string? RefreshTokenHash { get; set; }
+
+            public DateTime? RefreshTokenExpiryTime { get; set; }
+
+            public bool IsRefreshTokenRevoked { get; set; }
+        }
+    
+}
---
+++ Models/SupportTicket.cs
@@ -0,0 +1,19 @@
+﻿namespace NetWaveSupportAPI.Models
+{
+    public class SupportTicket
+    {
+
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
