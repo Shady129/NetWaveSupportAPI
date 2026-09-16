6329604f778d0cfc944e9b96c512e6a1552aa6f2
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-09-16 13:30:01 +0300

    Add files via upload

---
+++ DTOs/LogoutDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetWaveSupportAPI.DTOs
+{
+    public class LogoutDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ DTOs/RefreshTokenDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetWaveSupportAPI.DTOs
+{
+    public class RefreshTokenDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string RefreshToken { get; set; }
+    }
+}
