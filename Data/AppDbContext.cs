4f998bf7108bc4288468ff4ebae0ab3761c324d7
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-09-16 13:29:09 +0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetWaveSupportAPI.Models;
+
+namespace NetWaveSupportAPI.Data
+{
+ 
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Customer> Customers { get; set; }
+
+            public DbSet<SupportTicket> SupportTickets { get; set; }
+
+        }
+}
