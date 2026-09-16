ed10cd0c508e64eb6f03d7f0986cf26887176131
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-09-16 13:19:13 +0300

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NetWaveSupportAPI.Data;
+using NetWaveSupportAPI.DTOs;
+using NetWaveSupportAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace NetWaveSupportAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        private readonly IConfiguration _configuration;
+
+        private readonly ILogger<AuthController> _logger;
+
+
+        public AuthController(AppDbContext context, IConfiguration configuration, ILogger<AuthController> logger)
+        {
+            _context = context;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+
+        private string GenerateJwtToken(Customer customer)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, customer.Id.ToString()),
+            new Claim(ClaimTypes.Email, customer.Email),
+            new Claim(ClaimTypes.Role, customer.Role)
+    };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var credentials = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+        private string GenerateRefreshToken()
+        {
+            byte[] randomBytes = RandomNumberGenerator.GetBytes(64);
+
+            return Convert.ToBase64String(randomBytes);
+        }
+
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            bool emailExists = await _context.Customers
+                .AnyAsync(c => c.Email == dto.Email);
+
+            if (emailExists)
+                return BadRequest("Email already exists.");
+
+            string passwordHash =
+                BCrypt.Net.BCrypt.HashPassword(dto.Password);
+
+            Customer customer = new Customer
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                PasswordHash = passwordHash,
+                Role = "Customer",
+                IsRefreshTokenRevoked = false
+            };
+
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return Ok("Customer registered successfully.");
+        }
+
+
+        [EnableRateLimiting("fixed")]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            Customer? customer = await _context.Customers
+                .FirstOrDefaultAsync(c => c.Email == dto.Email);
+
+
+            if (customer == null)
+            {
+                _logger.LogWarning(
+                    "Failed login attempt for email {Email} from IP {IP}",
+                    dto.Email,
+                    HttpContext.Connection.RemoteIpAddress);
+
+                return Unauthorized("Invalid email or password.");
+            }
+
+
+            bool isPasswordValid =
+                BCrypt.Net.BCrypt.Verify(dto.Password, customer.PasswordHash);
+
+
+            if (!isPasswordValid)
+            {
+                _logger.LogWarning(
+                    "Failed login attempt for email {Email} from IP {IP}",
+                    dto.Email,
+                    HttpContext.Connection.RemoteIpAddress);
+
+                return Unauthorized("Invalid email or password.");
+            }
+
+
+            string token = GenerateJwtToken(customer);
+
+            string refreshToken = GenerateRefreshToken();
+
+            string refreshTokenHash = BCrypt.Net.BCrypt.HashPassword(refreshToken);
+
+
+            customer.RefreshTokenHash = refreshTokenHash;
+            customer.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+            customer.IsRefreshTokenRevoked = false;
+
+            await _context.SaveChangesAsync();
+
+
+            return Ok(new
+            {
+                AccessToken = token,
+                RefreshToken = refreshToken
+            });
+        }
+
+
+
+
+        [EnableRateLimiting("fixed")]
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh(RefreshTokenDto dto)
+        {
+
+            Customer? customer = await _context.Customers.FirstOrDefaultAsync(c => c.Email == dto.Email);
+
+            if (customer == null)
+            {
+                _logger.LogWarning(
+                    "Failed refresh attempt for email {Email} from IP {IP}",
+                    dto.Email,
+                    HttpContext.Connection.RemoteIpAddress);
+
+                return Unauthorized();
+            }
+
+
+            if (customer.IsRefreshTokenRevoked)
+            {
+                _logger.LogWarning(
+                    "Revoked refresh token used for email {Email} from IP {IP}",
+                    dto.Email,
+                    HttpContext.Connection.RemoteIpAddress);
+
+                return Unauthorized();
+            }
+
+
+
+            if (customer.RefreshTokenExpiryTime <= DateTime.UtcNow)
+            {
+                _logger.LogWarning(
+                    "Expired refresh token used for email {Email} from IP {IP}",
+                    dto.Email,
+                    HttpContext.Connection.RemoteIpAddress);
+
+                return Unauthorized();
+            }
+
+
+
+            bool isRefreshTokenValid = BCrypt.Net.BCrypt.Verify(dto.RefreshToken,customer.RefreshTokenHash);
+
+
+            if (!isRefreshTokenValid)
+            {
+                _logger.LogWarning(
+                    "Invalid refresh token used for email {Email} from IP {IP}",
+                    dto.Email,
+                    HttpContext.Connection.RemoteIpAddress);
+
+                return Unauthorized();
+            }
+
+
+
+
+            string newAccessToken = GenerateJwtToken(customer);
+
+            string newRefreshToken = GenerateRefreshToken();
+
+            string newRefreshTokenHash = BCrypt.Net.BCrypt.HashPassword(newRefreshToken);
+
+
+
+
+            customer.RefreshTokenHash = newRefreshTokenHash;
+            customer.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+            customer.IsRefreshTokenRevoked = false;
+
+
+            await _context.SaveChangesAsync();
+
+
+
+            return Ok(new
+            {
+                AccessToken = newAccessToken,
+                RefreshToken = newRefreshToken
+            });
+
+        }
+
+
+
+        [EnableRateLimiting("fixed")]
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout(LogoutDto dto)
+        {
+
+            Customer? customer = await _context.Customers.FirstOrDefaultAsync(c => c.Email == dto.Email);
+
+            if (customer == null)
+                return Unauthorized();
+
+
+
+            bool isRefreshTokenValid = BCrypt.Net.BCrypt.Verify(dto.RefreshToken,customer.RefreshTokenHash);
+
+            if (!isRefreshTokenValid)
+                return Unauthorized();
+
+
+            customer.IsRefreshTokenRevoked = true;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Logged out successfully.");
+
+        }
+
+    }
+}
---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetWaveSupportAPI.Data;
+using System.Security.Claims;
+using NetWaveSupportAPI.DTOs;
+using NetWaveSupportAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetWaveSupportAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TicketsController : ControllerBase
+    {
+
+
+
+        private readonly AppDbContext _context;
+
+        private readonly ILogger<TicketsController> _logger;
+
+        public TicketsController(AppDbContext context, ILogger<TicketsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(CreateTicketDto dto)
+        {
+            int customerId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            SupportTicket ticket = new SupportTicket
+            {
+                CustomerId = customerId,
+                Title = dto.Title,
+                Description = dto.Description,
+                Status = "Open",
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _context.SupportTickets.Add(ticket);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTicketById),new { id = ticket.Id },ticket);
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyTickets()
+        {
+            int customerId = int.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var tickets = await _context.SupportTickets
+                .Where(t => t.CustomerId == customerId)
+                .ToListAsync();
+
+            return Ok(tickets);
+        }
+
+
+
+        [HttpGet("all")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllTickets()
+        {
+            var tickets = await _context.SupportTickets.ToListAsync();
+
+            return Ok(tickets);
+        }
+
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTicketById(int id)
+        {
+            var ticket = await _context.SupportTickets
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (ticket == null)
+                return NotFound();
+
+            int customerId = int.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+
+            string role = User.FindFirstValue(ClaimTypes.Role);
+
+
+            if (ticket.CustomerId != customerId && role != "Admin")
+            {
+                _logger.LogWarning(
+                    "Forbidden access attempt by User {UserId} to Ticket {TicketId}",
+                    customerId,
+                    ticket.Id);
+
+                return Forbid();
+            }
+
+            return Ok(ticket);
+        }
+
+
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTicket(int id, UpdateTicketDto dto)
+        {
+            var ticket = await _context.SupportTickets
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (ticket == null)
+                return NotFound();
+
+            int customerId = int.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            string role = User.FindFirstValue(ClaimTypes.Role);
+
+            if (ticket.CustomerId != customerId && role != "Admin")
+            {
+                _logger.LogWarning(
+                    "Forbidden access attempt by User {UserId} to Ticket {TicketId}",
+                    customerId,
+                    ticket.Id);
+
+                return Forbid();
+            }
+
+            ticket.Title = dto.Title;
+            ticket.Description = dto.Description;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(ticket);
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTicket(int id)
+        {
+            var ticket = await _context.SupportTickets
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (ticket == null)
+                return NotFound();
+
+            int customerId = int.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            string role = User.FindFirstValue(ClaimTypes.Role);
+
+
+            if (ticket.CustomerId != customerId && role != "Admin")
+            {
+                _logger.LogWarning(
+                    "Forbidden access attempt by User {UserId} to Ticket {TicketId}",
+                    customerId,
+                    ticket.Id);
+
+                return Forbid();
+            }
+
+    
+
+           _context.SupportTickets.Remove(ticket);
+            await _context.SaveChangesAsync();
+
+
+
+            _logger.LogInformation("Ticket {TicketId} deleted by User {UserId}",ticket.Id,
+            User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+
+            return NoContent();
+        }
+    }
+}
